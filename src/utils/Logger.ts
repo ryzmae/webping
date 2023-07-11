311b9f5ffe54fa7cf2810b96ae65ad3aa9e3f996
ryzmae <contact@ryzm.tech>
2023-07-11 20:47:28 +0200

    update logger and added WebPing.ts

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,4 +1,4 @@
-import * as chalk from 'chalk';
+import chalk from 'chalk';
 import moment from 'moment';
 
 const logs = {
@@ -30,6 +30,53 @@ export class Logger {
     private GetDate() {
         return moment().format("DD/MM/YY HH:mm:ss");
     }
-
     
+    info(...input: string[]) {
+        if (!this.options.info) return;
+        return console.log([
+            chalk.gray.bold(this.GetDate()) + " ",
+            this.prefix ? chalk.yellow.bold(this.prefix) : "",
+            this.spaces,
+            chalk.cyan.bold(logs.Info),
+            chalk.cyan.bold("|| "),
+            chalk.cyan.dim(input.flat().join(" "))
+        ].join(""))
+    }
+
+    error(error: Error) {
+        if (!this.options.error) return;
+        return console.log([
+            chalk.gray.bold(this.GetDate()) + " ",
+            this.prefix ? chalk.yellow.bold(this.prefix) : "",
+            this.spaces,
+            chalk.red.bold(logs.Error),
+            chalk.red.bold("|| "),
+            chalk.red.dim((error.stack ? [error.stack] : [error.name, error.message]).filter(Boolean).map(v => v.toString()).join("\n"))
+        ].join(""))
+    }
+
+    debug(...input: string[]) {
+        if (!this.options.debug) return;
+        return console.log([
+            chalk.gray.bold(this.GetDate()) + " ",
+            this.prefix ? chalk.yellow.bold(this.prefix) : "",
+            this.spaces,
+            chalk.gray.bold(logs.Debug),
+            chalk.gray.bold("|| "),
+            chalk.gray.dim(input.flat().join(" "))
+        ].join(""))
+    }
+
+    warn(...input: string[]) {
+        if (!this.options.warn) return;
+        return console.log([
+            chalk.gray.bold(this.GetDate()) + " ",
+            this.prefix ? chalk.yellow.bold(this.prefix) : "",
+            this.spaces,
+            chalk.redBright.bold(logs.Warn),
+            chalk.redBright.bold("|| "),
+            chalk.redBright.dim(input.flat().join(" "))
+        ].join(""))
+    }
+
 }
--- src/webping.ts
+++ src/webping.ts
@@ -1,1 +1,116 @@
-import * as http from "http";
+import * as http from 'http';
+import * as https from 'https';
+
+import { Logger } from './utils/Logger';
+
+const logger: any = new Logger({
+    info: true,
+    error: true,
+    warn: true,
+    debug: true
+});
+
+class httpWebPing {
+    private timerId: NodeJS.Timeout | null;
+    private websiteUrl: string;
+    private interval: number;
+
+    constructor(websiteUrl: string, interval: number) {
+        this.timerId = null;
+        this.websiteUrl = websiteUrl;
+        this.interval = Math.max(1, Math.min(5, interval));
+    }
+
+    start(): void {
+        if (this.timerId) {
+            logger.warn("Timer already started");
+            return;
+        }
+
+        logger.info("WebPing started");
+        
+        this.timerId = setInterval(() => {
+            this.pingWebsite();
+        }, this.interval * 1000);
+    }
+
+    stop(): void {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+            logger.info("WebPing stopped");
+        } else {
+            logger.warn("WebPing is not started");
+        }
+    }
+
+    private pingWebsite(): void {
+        http.get(this.websiteUrl, (res: http.IncomingMessage) => {
+            const isWebsiteUp: boolean = res.statusCode === 200;
+
+            if (isWebsiteUp) {
+                logger.info(`Website ${this.websiteUrl} is up`);
+            } else {
+                logger.error(`Website ${this.websiteUrl} is down`);
+            }
+        })
+
+        .on('error', (err: Error) => {
+            logger.error(`An error occurred \n ${err.message}`);
+        })
+    }
+}
+
+class httpsWebPing {
+    private timerId: NodeJS.Timeout | null;
+    private websiteUrl: string;
+    private interval: number;
+
+    constructor(websiteUrl: string, interval: number) {
+        this.timerId = null;
+        this.websiteUrl = websiteUrl;
+        this.interval = Math.max(1, Math.min(5, interval));
+
+    }
+
+    start(): void {
+        if (this.timerId) {
+            logger.warn("Timer already started");
+            return;
+        }
+
+        logger.info("WebPing started");
+        
+        this.timerId = setInterval(() => {
+            this.pingWebsite();
+
+        }, this.interval * 1000);
+    }
+
+    stop(): void {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+            logger.info("WebPing stopped");
+
+        } else {
+            logger.warn("WebPing is not started");
+        }
+    }
+
+    private pingWebsite(): void {
+        https.get(this.websiteUrl, (res: http.IncomingMessage) => {
+            const isWebsiteUp: boolean = res.statusCode === 200;
+
+            if (isWebsiteUp) {
+                logger.info(`Website ${this.websiteUrl} is up`);
+            } else {
+                logger.error(`Website ${this.websiteUrl} is down`);
+            }
+        })
+
+        .on('error', (err: Error) => {
+            logger.error(`An error occurred \n ${err.message}`);
+        })
+    }
+}
