4640d02e6f09d125105aea291c63796d24f791cf
ryzmae <contact@ryzm.tech>
2023-07-11 19:24:38 +0200

    adding custom Logger to manage the logs easier

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,2 +1,35 @@
 import * as chalk from 'chalk';
-import * as moment from 'moment';
+import moment from 'moment';
+
+const logs = {
+    Info: "   Info",
+    Error: "  Error",
+    Warn: "   Warn",
+    Debug: "  Debug ",
+}
+
+export interface LoggerOptions {
+    info?: boolean;
+    error?: boolean;
+    warn?: boolean;
+    debug?: boolean;
+}
+
+export class Logger {
+    private options: LoggerOptions = {
+        info: true,
+        error: true,
+        warn: true,
+        debug: true,
+    };
+    private spaces: string = chalk.magenta.bold(" :: ");
+    private prefix: string = "Logger";
+    constructor(options: LoggerOptions) {
+        this.options = { ...this.options, ...options };
+    }
+    private GetDate() {
+        return moment().format("DD/MM/YY HH:mm:ss");
+    }
+
+    
+}
